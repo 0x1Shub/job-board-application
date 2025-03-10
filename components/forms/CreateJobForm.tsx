c6a7d99dd877dbc69d54be79174d52c1e68d3657
0x1Shub <shubhamyeram13@gmail.com>
2025-03-10 15:43:25 +0530

    Post Job

--- components/forms/CreateJobForm.tsx
+++ components/forms/CreateJobForm.tsx
@@ -12,20 +12,38 @@ import { countryList } from "@/app/utils/countriesList";
 import { SalaryRangeSelector } from "../general/SalaryRangeSelector";
 import { JobDescriptionEditor } from "../richTextEditor/JobDescriptionEditor";
 import { BenefitsSelector } from "../general/BenefitsSelector";
+import { Textarea } from "../ui/textarea";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import { XIcon } from "lucide-react";
+import { UploadDropzone } from "../general/UploadThingReexported";
+import { JobListingDuration } from "../general/JobListingDurationSelector";
+import { createJob } from "@/app/actions";
+import { useState } from "react";
 
-export function CreateJobForm(){
+interface iAppProps {
+    companyName: string;
+    companyLocation: string;
+    companyAbout: string;
+    companyLogo: string;
+    companyWebsite: string;
+    companyXAccount: string | null;
+    companyLinkedInAccount: string | null;
+}
+
+export function CreateJobForm({companyAbout, companyLinkedInAccount, companyLocation, companyLogo, companyName, companyWebsite, companyXAccount} : iAppProps){
 
     const form = useForm<z.infer<typeof jobSchema>>({
         resolver: zodResolver(jobSchema),
         defaultValues: {
             benefits: [],
-            companyAbout: "",
-            companyLocation: "",
-            companyName: "",
-            companyLogo: "",
-            companyWebsite: "",
-            companyXAccount: "",
-            companyLinkedInAccount: "",
+            companyAbout: companyAbout,
+            companyLocation: companyLocation,
+            companyName: companyName,
+            companyLogo: companyLogo,
+            companyWebsite: companyWebsite,
+            companyXAccount: companyXAccount || "",
+            companyLinkedInAccount: companyLinkedInAccount || "",
             employmentType: "",
             jobDescription: "",
             jobTitle: "",
@@ -34,16 +52,32 @@ export function CreateJobForm(){
             salaryFrom: 0,
             salaryTo: 0,
         }
-    })
+    });
+
+    const [pending, setPending] = useState(false);
+
+    async function onSubmit(values: z.infer<typeof jobSchema>){
+        try {
+            setPending(true);
+            await createJob(values);
+        }catch(error){
+            if(error instanceof Error && error.message !== 'NEXT_REDIRECT'){
+                console.log('Something went wrong');
+            }
+        }
+        finally {
+            setPending(false);
+        }
+    }
 
     return (
         <Form {...form}>
-            <form className="col-span-1 lg:col-span-2 flex flex-col gap-8">
+            <form onSubmit={form.handleSubmit(onSubmit)} className="col-span-1 lg:col-span-2 flex flex-col gap-8">
                 <Card>
                     <CardHeader>
                         <CardTitle>Job Information</CardTitle>
                     </CardHeader>
-                    <CardContent>
+                    <CardContent className="space-y-6">
                         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
                             <FormField
                                 control={form.control}
@@ -166,7 +200,199 @@ export function CreateJobForm(){
                         /> 
                     </CardContent>
                 </Card>
+
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Company Information</CardTitle>
+                    </CardHeader>
+                    <CardContent className="space-y-6">
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <FormField
+                                control={form.control}
+                                name="companyName"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Company Name</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your company's name..." {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} 
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="companyLocation"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Company Location</FormLabel>
+                                        <Select onValueChange={field.onChange} defaultValue={field.value}>
+                                            <FormControl>
+                                                <SelectTrigger>
+                                                    <SelectValue placeholder="Select a location..." />
+                                                </SelectTrigger>
+                                            </FormControl>
+                                            <SelectContent>
+                                                <SelectGroup>
+                                                    <SelectLabel>Worldwide</SelectLabel>
+                                                    <SelectItem value="worldwide">
+                                                        <span>🌏</span> <span  className="pl-2">Worldwide / Remote</span>
+                                                    </SelectItem>
+                                                </SelectGroup>
+                                                <SelectGroup>
+                                                    <SelectLabel>Location</SelectLabel>
+                                                    {countryList.map((country) => (
+                                                        <SelectItem key={country.code} value={country.name}>
+                                                            <span>{country.flagEmoji}</span>
+                                                            <span className="ml-2">{country.name}</span>
+                                                        </SelectItem>
+                                                    ))}
+                                                </SelectGroup>
+                                            </SelectContent>
+                                        </Select>
+                                        <FormMessage  />
+                                    </FormItem>
+                                )}
+                            />
+                        </div>
+
+                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+                            <FormField
+                                control={form.control}
+                                name="companyWebsite"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Company Website</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your company's official website..." {...field} />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} 
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="companyXAccount"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Company X (Twitter) Account</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="@yourcompany" {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )} 
+                            />
+
+                            <FormField
+                                control={form.control}
+                                name="companyLinkedInAccount"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>Company LinkedIn Profile</FormLabel>
+                                        <FormControl>
+                                            <Input placeholder="Enter your company's LinkedIn profile URL..." {...field} />
+                                        </FormControl>
+                                    </FormItem>
+                                )} 
+                            />
+
+                            
+                        </div>
+
+                        <FormField
+                                control={form.control}
+                                name="companyAbout"
+                                render={({field}) => (
+                                    <FormItem>
+                                        <FormLabel>About the Company</FormLabel>
+                                        <FormControl>
+                                            <Textarea 
+                                                placeholder="Provide a brief overview of your company's mission, culture, and industry..." 
+                                                {...field}
+                                                className="min-h-[120px]" 
+                                            />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )} 
+                            />
+
+                        <FormField
+                            control={form.control}
+                            name="companyLogo"
+                            render={({ field }) => (
+                                <FormItem>
+                                    <FormLabel>Company Logo</FormLabel>
+                                    <FormControl>
+                                        <div>
+                                            {
+                                                field.value ? (
+                                                    <div className="relative w-fit">
+                                                        <Image 
+                                                            src={field.value} 
+                                                            alt="Company Logo" 
+                                                            width={100} 
+                                                            height={100} 
+                                                            className="rounded-lg"
+                                                        />
+                                                        <Button
+                                                            type="button"
+                                                            variant={'destructive'}
+                                                            size={'icon'}
+                                                            className="absolute -top-2 -right-2"
+                                                            onClick={() => field.onChange("")}
+                                                        >
+                                                            <XIcon className="size-4" />
+                                                        </Button>
+                                                    </div>
+                                                ) : (
+                                                    <UploadDropzone endpoint="imageUploader"
+                                                        onClientUploadComplete={(res) => {
+                                                            field.onChange(res[0].url);
+                                                        }} 
+                                                        onUploadError={() => {
+                                                            console.log("An error occurred while uploading. Please try again.");
+                                                        }}
+                                                        className="ut-button:bg-primary ut-button:text-white ut-button:hover:bg-primary/90
+                                                        ut-label:text-muted-foreground ut-allowed-content:text-muted-foreground border-primary"
+                                                    />
+                                                )
+                                            }
+                                        </div>
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                </Card>
+
+                <Card>
+                    <CardHeader>
+                        <CardTitle>Job Listing Duration</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <FormField 
+                            control={form.control}
+                            name="listingDuration"
+                            render={({field}) => (
+                                <FormItem>
+                                    <FormControl>
+                                        <JobListingDuration field={field as any} />
+                                    </FormControl>
+                                    <FormMessage />
+                                </FormItem>
+                            )}
+                        />
+                    </CardContent>
+                </Card>
+
+                <Button type="submit" className="w-full" disabled={pending}>
+                    {pending ? "Submitting" : "Create Job Post"}
+                </Button>
             </form>
         </Form>
     )
-}
+} 
