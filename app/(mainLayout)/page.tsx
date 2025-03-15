6b8f6217806b123b8b879c2f29047772fa1d2dc5
0x1Shub <shubhamyeram13@gmail.com>
2025-03-15 15:30:51 +0530

    Job Filters

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -1,7 +1,14 @@
+import { JobFilter } from "@/components/general/JobFilters";
+import { Card } from "@/components/ui/card";
+
 export default function Home() {
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="grid grid-cols-3 gap-8">
+      <JobFilter />
+
+      <Card className="col-span-2">
+
+      </Card>
     </div>
   );
 }
