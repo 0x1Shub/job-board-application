4424b6eca54396191c7da5fa83211a42b4cbcc77
0x1Shub <shubhamyeram13@gmail.com>
2025-03-16 15:26:55 +0530

    Empty State

--- app/(mainLayout)/page.tsx
+++ app/(mainLayout)/page.tsx
@@ -1,4 +1,5 @@
 import { JobFilter } from "@/components/general/JobFilters";
+import { JobListings } from "@/components/general/JobListings";
 import { Card } from "@/components/ui/card";
 
 export default function Home() {
@@ -6,9 +7,9 @@ export default function Home() {
     <div className="grid grid-cols-3 gap-8">
       <JobFilter />
 
-      <Card className="col-span-2">
-
-      </Card>
+      <div className="col-span-2 flex flex-col gap-6"> 
+          <JobListings />
+      </div>
     </div>
   );
 }
